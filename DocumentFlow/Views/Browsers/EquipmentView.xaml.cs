30356b696d5990cfe1a9aa9abaa972ab6dc2f840
Тепляшин Сергей Васильевич <sergio.teplyashin@yandex.ru>
2024-02-12 19:21:45 +0400

    Добавлена многострочность для заголовков

--- DocumentFlow/Views/Browsers/EquipmentView.xaml.cs
+++ DocumentFlow/Views/Browsers/EquipmentView.xaml.cs
@@ -17,10 +17,26 @@ namespace DocumentFlow.Views.Browsers;
 /// </summary>
 public partial class EquipmentView : UserControl, IGridPageView, ISelfTransientLifetime
 {
+    private readonly GridRowSizingOptions gridRowResizingOptions = new();
     public EquipmentView()
     {
         InitializeComponent();
     }
 
     public SfDataGrid DataGrid => gridContent;
+
+    private void GridContent_QueryRowHeight(object sender, QueryRowHeightEventArgs e)
+    {
+        if (gridContent.GetHeaderIndex() == e.RowIndex)
+        {
+            if (gridContent.GridColumnSizer.GetAutoRowHeight(e.RowIndex, gridRowResizingOptions, out var autoHeight))
+            {
+                if (autoHeight > 24)
+                {
+                    e.Height = autoHeight;
+                    e.Handled = true;
+                }
+            }
+        }
+    }
 }
