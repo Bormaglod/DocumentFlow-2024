d40273bea2effbf13cbdf072e91f74c91b8b9dec
Тепляшин Сергей Васильевич <sergio.teplyashin@yandex.ru>
2024-02-13 23:33:02 +0400

    Добавлен просмотр списка произрдственных операций

---
+++ DocumentFlow/Models/Entities/Operation.cs
@@ -0,0 +1,52 @@
+﻿//-----------------------------------------------------------------------
+// Copyright © 2010-2024 Тепляшин Сергей Васильевич. 
+// Contacts: <sergio.teplyashin@yandex.ru>
+// License: https://opensource.org/licenses/GPL-3.0
+//-----------------------------------------------------------------------
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DocumentFlow.Models.Entities;
+
+public partial class Operation : Directory
+{
+    /// <summary>
+    /// Возвращает или устанавливает выработка за указанное время <see cref="ProdTime"/>.
+    /// </summary>
+    [ObservableProperty]
+    private int produced;
+
+    /// <summary>
+    /// Возвращает или устанавливает время за которое было произведено указанное количество операций <see cref="Produced"/> (указывается в минутах).
+    /// </summary>
+    [ObservableProperty]
+    private int prodTime;
+
+    /// <summary>
+    /// Возвращает или устанавливает норму выработки (шт./час).
+    /// </summary>
+    [ObservableProperty]
+    private int productionRate;
+
+    //private Guid typeId;
+
+    /// <summary>
+    /// Возвращает или устанавливает дату нормирования.
+    /// </summary>
+    [ObservableProperty]
+    private DateTime? dateNorm;
+
+    /// <summary>
+    /// Возвращает или устанавливает плату за выполнение ед. операции
+    /// </summary>
+    [ObservableProperty]
+    private decimal salary;
+
+    /// <summary>
+    /// Возвращает или устанавливает тип операции <seealso cref="OperationTypes.OperationType"/>.
+    /// </summary>
+    [ObservableProperty]
+    private OperationType? operationType;
+
+    public bool OperationUsing { get; protected set; }
+}
---
+++ DocumentFlow/ViewModels/Browsers/OperationViewModel.cs
@@ -0,0 +1,49 @@
+﻿//-----------------------------------------------------------------------
+// Copyright © 2010-2024 Тепляшин Сергей Васильевич. 
+// Contacts: <sergio.teplyashin@yandex.ru>
+// License: https://opensource.org/licenses/GPL-3.0
+//-----------------------------------------------------------------------
+
+using DocumentFlow.Common.Data;
+using DocumentFlow.Interfaces;
+using DocumentFlow.Models.Entities;
+
+using SqlKata;
+
+using System.Data;
+
+namespace DocumentFlow.ViewModels.Browsers;
+
+public class OperationViewModel : DirectoryViewModel<Operation>, ISelfTransientLifetime
+{
+    public OperationViewModel() { }
+
+    public OperationViewModel(IDatabase database) : base(database) { }
+
+    //public override Type? GetEditorViewType() => typeof(Views.Editors.MeasurementView);
+
+    protected override void ConfigureColumn(IColumnInfo columnInfo)
+    {
+        if (columnInfo.MappingName == nameof(Directory.ItemName))
+        {
+            columnInfo.AlwaysVisible = true;
+        }
+    }
+
+    protected override Query SelectQuery(Query query)
+    {
+        return base.SelectQuery(query)
+            .SelectRaw("exists(select 1 from calculation_operation co join calculation c ON c.id = co.owner_id where co.item_id = t0.id and c.state = 'approved'::calculation_state) as operation_using");
+    }
+
+    protected override IReadOnlyList<Operation> GetData(IDbConnection connection, Guid? id = null)
+    {
+        return GetData<OperationType>(connection, (operation, type) =>
+        {
+            operation.OperationType = type;
+            return operation;
+        },
+        refs => "type_id",
+        parameters: new QueryParemeters() { FromOnly = true });
+    }
+}
