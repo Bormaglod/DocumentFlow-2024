efb617e3d6f554f6faa4ca2819ce2e9b266761e4
Тепляшин Сергей Васильевич <sergio.teplyashin@yandex.ru>
2024-02-21 19:51:27 +0400

    "Модели производственных операций и резки наследуются от одного класса BaseOperationViewModel"
    This reverts commit 4489a18acba1d14c8ce562fb783a1a088e9d6045.

---
+++ DocumentFlow/Models/Entities/CalculationMaterial.cs
@@ -0,0 +1,56 @@
+﻿//-----------------------------------------------------------------------
+// Copyright © 2010-2024 Тепляшин Сергей Васильевич. 
+// Contacts: <sergio.teplyashin@yandex.ru>
+// License: https://opensource.org/licenses/GPL-3.0
+//-----------------------------------------------------------------------
+
+namespace DocumentFlow.Models.Entities;
+
+public class CalculationMaterial : CalculationItem
+{
+    /*private decimal amount;
+    private bool isGiving;
+    private string priceSettingMethod = "average";
+
+    /// <summary>
+    /// Возвращает или устанавливает расход материала на изделие.
+    /// </summary>
+    public decimal Amount 
+    { 
+        get => amount;
+        set => SetProperty(ref amount, value);
+    }
+
+    /// <summary>
+    /// Возвращает или устанавливает флаг определяющий - является ли материал давальческим.
+    /// </summary>
+    public bool IsGiving 
+    { 
+        get => isGiving;
+        set => SetProperty(ref isGiving, value);
+    }
+
+    [EnumType("price_setting_method")]
+    public string PriceSettingMethod 
+    { 
+        get => priceSettingMethod;
+        set => SetProperty(ref priceSettingMethod, value);
+    }
+
+    [Computed]
+    public string? MaterialName { get; protected set; }
+
+    [Computed]
+    public decimal Weight { get; protected set; }
+
+    [Write(false)]
+    public PriceSettingMethod MethodPrice
+    {
+        get { return Enum.Parse<PriceSettingMethod>(PriceSettingMethod.Pascalize()); }
+        set { PriceSettingMethod = value.ToString().Underscore(); }
+    }
+
+    public string MethodPriceName => MethodPrice.Description();
+
+    public override string ToString() => MaterialName ?? "[NULL]";*/
+}
---
+++ DocumentFlow/ViewModels/Editors/BaseOperationViewModel.cs
@@ -0,0 +1,65 @@
+﻿//-----------------------------------------------------------------------
+// Copyright © 2010-2024 Тепляшин Сергей Васильевич. 
+// Contacts: <sergio.teplyashin@yandex.ru>
+// License: https://opensource.org/licenses/GPL-3.0
+//-----------------------------------------------------------------------
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+using DocumentFlow.Models.Entities;
+
+namespace DocumentFlow.ViewModels.Editors;
+
+public abstract partial class BaseOperationViewModel<T> : DirectoryEditorViewModel<T>
+    where T : Operation, new()
+{
+    [ObservableProperty]
+    private string code = string.Empty;
+
+    [ObservableProperty]
+    private string? itemName;
+
+    [ObservableProperty]
+    private int produced;
+
+    [ObservableProperty]
+    private int prodTime;
+
+    [ObservableProperty]
+    private int productionRate;
+
+    [ObservableProperty]
+    private DateTime? dateNorm;
+
+    [ObservableProperty]
+    private decimal salary;
+
+    protected override void RaiseAfterLoadDocument(T entity)
+    {
+        Code = entity.Code;
+        ParentId = entity.ParentId;
+        ItemName = entity.ItemName;
+        Produced = entity.Produced;
+        ProdTime = entity.ProdTime;
+        ProductionRate = entity.ProductionRate;
+        DateNorm = entity.DateNorm;
+        Salary = entity.Salary;
+    }
+
+    protected override void UpdateEntity(T entity)
+    {
+        entity.Code = Code;
+        entity.ParentId = ParentId;
+        entity.ItemName = ItemName;
+        entity.Produced = Produced;
+        entity.ProdTime = ProdTime;
+        entity.ProductionRate = ProductionRate;
+        entity.DateNorm = DateNorm;
+        entity.Salary = Salary;
+    }
+
+    partial void OnItemNameChanged(string? value)
+    {
+        UpdateHeader(value ?? "?");
+    }
+}
