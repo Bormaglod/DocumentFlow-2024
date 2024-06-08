e0f6dc404899e3e78ef2fda5d8efe4c574884d78
Тепляшин Сергей Васильевич <sergio.teplyashin@yandex.ru>
2024-06-08 22:02:39 +0400

    Issues #8

--- DocumentFlow/ViewModels/Browsers/WaybillReceiptViewModel.cs
+++ DocumentFlow/ViewModels/Browsers/WaybillReceiptViewModel.cs
@@ -4,6 +4,7 @@
 // License: https://opensource.org/licenses/GPL-3.0
 //-----------------------------------------------------------------------
 
+using DocumentFlow.Common.Data;
 using DocumentFlow.Common.Extensions;
 using DocumentFlow.Interfaces;
 using DocumentFlow.Interfaces.Repository;
@@ -34,35 +35,49 @@ public class WaybillReceiptViewModel : WaybillViewModel<WaybillReceipt>, ISelfTr
 
     protected override Query SelectQuery(Query query)
     {
-        var q = new Query("waybill_receipt_price")
+        var wrp = new Query("waybill_receipt_price")
             .Select("owner_id")
             .SelectRaw("sum(product_cost) as product_cost")
             .SelectRaw("sum(tax_value) as tax_value")
             .SelectRaw("sum(full_cost) as full_cost")
             .GroupBy("owner_id");
 
-        var ppr = GetQuery("posting_payments_receipt", "document_id");
-        var ppp = GetQuery("posting_payments_purchase", "document_id");
+        var receipt = GetQuery("posting_payments_receipt", "document_id");
+        var purchase = GetQuery("posting_payments_purchase", "document_id");
         var debt = GetQuery("debt_adjustment", "document_debt_id");
         var credit = GetQuery("debt_adjustment", "document_credit_id");
 
-        return base
-            .SelectQuery(query)
+        var wbr = new Query("waybill_receipt as wr")
+            .Select("wr.*")
             .Select("d.{product_cost, tax_value, full_cost}")
-            .SelectRaw("coalesce(ppr.transaction_amount, 0) + coalesce(ppp.transaction_amount, 0) + coalesce(credit.transaction_amount, 0) - coalesce(debt.transaction_amount, 0) as paid")
+            .SelectRaw("coalesce(ppr.transaction_amount, 0) + coalesce(ppp.transaction_amount, 0) + coalesce(crdt.transaction_amount, 0) - coalesce(dbt.transaction_amount, 0) as paid")
             .Select("pr.document_number as purchase_request_number")
             .Select("pr.document_date as purchase_request_date")
-            .LeftJoin("purchase_request as pr", "pr.id", "t0.owner_id")
-            .LeftJoin(q.As("d"), j => j.On("d.owner_id", "t0.id"))
-            .LeftJoin(ppr.As("ppr"), j => j.On("ppr.document_id", "t0.id"))
-            .LeftJoin(ppp.As("ppp"), j => j.On("ppp.document_id", "pr.id"))
-            .LeftJoin(debt.As("debt"), j => j.On("debt.document_debt_id", "t0.id"))
-            .LeftJoin(credit.As("credit"), j => j.On("credit.document_credit_id", "t0.id"));
+            .LeftJoin("purchase_request as pr", "pr.id", "wr.owner_id")
+            .LeftJoin("wrp as d", "d.owner_id", "wr.id")
+            .LeftJoin("receipt as ppr", "ppr.document_id", "wr.id")
+            .LeftJoin("purchase as ppp", "ppp.document_id", "pr.id")
+            .LeftJoin("debt as dbt", "dbt.document_debt_id", "wr.id")
+            .LeftJoin("credit as crdt", "crdt.document_credit_id", "wr.id");
+
+        return base
+            .SelectQuery(query)
+            .With("wrp", wrp)
+            .With("receipt", receipt)
+            .With("purchase", purchase)
+            .With("debt", debt)
+            .With("credit", credit)
+            .With("wbr", wbr);
     }
 
     protected override IReadOnlyList<WaybillReceipt> GetData(IDbConnection connection, Guid? id = null)
     {
-        return DefaultQuery(connection, id)
+        QueryParameters parameters = new()
+        {
+            Table = "wbr"
+        };
+
+        return DefaultQuery(connection, id, parameters)
             .Get<WaybillReceipt, State, Contractor, Contract>(
                 (pr, state, contractor, contract) =>
                 {
@@ -88,5 +103,12 @@ public class WaybillReceiptViewModel : WaybillViewModel<WaybillReceipt>, ISelfTr
         base.OnCopyNestedRows(connection, from, to, transaction);
 
         requestRepository.CopyContent(connection, from, to, transaction);
+    }
+
+    protected override Query FilterQuery(Query query)
+    {
+        return base
+            .FilterQuery(query)
+            .OrWhereColumns("paid", "!=", "full_cost");
     }
 }
